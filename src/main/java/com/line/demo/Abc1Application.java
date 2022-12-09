2473ed41d2374154f5b5e011e50481a6f96859a9
jeongzmin <jeongzmin@plateer.com>
2022-12-09 09:05:35 +0900

    test

---
+++ src/main/java/com/line/demo/Abc1Application.java
@@ -0,0 +1,13 @@
+package com.line.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Abc1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Abc1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/line/demo/Abc1ApplicationTests.java
@@ -0,0 +1,14 @@
+package com.line.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Abc1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("..");
+	}
+
+}
